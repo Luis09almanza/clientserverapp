b64a4dbecc804dc0a8de59cbd4e0f7a0ec248c0d
Luis Hernandez <luis09almanza@gmail.com>
2022-05-17 13:16:55 -0600

    feat: two method post and method get modified

--- server.py
+++ server.py
@@ -1,14 +1,34 @@
-''' Import the libraries '''
 from flask import Flask, request
 
-''' This is to run our application with flask'''
 app = Flask('__main__')
 
-''' This is the route of our application with the method in this case GET ''' 
-@app.route('/', methods=['GET'])
-def go_home():
-    return 'Hola mundo'
+'''
+Now we put this diccionary to see in postman.
+'''
+device = {
+    "code":"11233",
+    "descrip":"Temp. Sensor",
+    "value":67
+}
 
-''' We defined a point of entrance to run our application in the port 5000 '''
+'''Now the route is changed and the return too '''
+@app.route('/devices', methods=['GET'])
+def go_home():
+    return device
+
+''' This is to save a user '''
+@app.route('/users', methods=['POST'])
+def save_user():
+    user = request.json
+    print(user)
+    return user
+
+''' This is to save a device '''
+@app.route('/devices', methods=['POST'])
+def save_device():
+    device = request.json
+    return device
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
